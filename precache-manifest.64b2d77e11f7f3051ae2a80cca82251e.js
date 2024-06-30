8ee917574f614a4ed3c847adb8f98c7378721175
mattdahse <N3wy0rkminute>
2024-06-30 16:51:58 -0700

    Updates

--- precache-manifest.64b2d77e11f7f3051ae2a80cca82251e.js
+++ precache-manifest.64b2d77e11f7f3051ae2a80cca82251e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea09a3f6c77066c10ad4230f7e042295",
+    "revision": "bf18bb1e84a72642621df439f9fa888c",
     "url": "/absev-enordfor-ai/index.html"
   },
   {
-    "revision": "c2d67b2dc67283d16b76",
+    "revision": "866a870e0c3d243b76e4",
     "url": "/absev-enordfor-ai/static/css/main.cae776dd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/absev-enordfor-ai/static/js/3.a29efab2.chunk.js"
   },
   {
-    "revision": "c2d67b2dc67283d16b76",
-    "url": "/absev-enordfor-ai/static/js/main.2596e219.chunk.js"
+    "revision": "866a870e0c3d243b76e4",
+    "url": "/absev-enordfor-ai/static/js/main.eaa9882b.chunk.js"
   },
   {
     "revision": "96385e7be42e3739be99",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/absev-enordfor-ai/precache-manifest.03b284a072c67ecce3506bdda039bd88.js"
+  "/absev-enordfor-ai/precache-manifest.64b2d77e11f7f3051ae2a80cca82251e.js"
 );
 
 self.addEventListener('message', (event) => {
